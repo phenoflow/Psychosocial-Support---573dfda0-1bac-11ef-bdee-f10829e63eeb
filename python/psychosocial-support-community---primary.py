b69fa61c25bf9ddb8083c5e2ad2bdefe9b44df5f
Martin Chapman <contact@martinchapman.co.uk>
2024-06-02 21:38:53 +0100

    Update made by Phenoflow

--- python/psychosocial-support-community---primary.py
+++ python/psychosocial-support-community---primary.py
@@ -2,11 +2,11 @@
 
 import sys, csv, re
 
-codes = [{"code":"9k11.00","system":"readv2"},{"code":"9k14.00","system":"readv2"},{"code":"Z4B1.00","system":"readv2"}];
+codes = [{"code":"9NN2.00","system":"readv2"},{"code":"8H7p.00","system":"readv2"},{"code":"8HHe.00","system":"readv2"},{"code":"8IEA.00","system":"readv2"}];
 REQUIRED_CODES = 1;
 with open(sys.argv[1], 'r') as file_in, open('psychosocial-support-potential-cases.csv', 'w', newline='') as file_out:
     csv_reader = csv.DictReader(file_in)
-    csv_writer = csv.DictWriter(file_out, csv_reader.fieldnames + ["psychosocial-support-counselling---primary-identified"])
+    csv_writer = csv.DictWriter(file_out, csv_reader.fieldnames + ["psychosocial-support-community---primary-identified"])
     csv_writer.writeheader();
     codes_identified = 0;
     for row in csv_reader:
@@ -16,10 +16,10 @@ with open(sys.argv[1], 'r') as file_in, open('psychosocial-support-potential-cas
             for item in re.findall(r'\(([^,]*)\,', row[cell]):
                 if(item in list(map(lambda code: code['code'], codes))): codes_identified+=1;
                 if(codes_identified>=REQUIRED_CODES):
-                    newRow["psychosocial-support-counselling---primary-identified"] = "CASE";
+                    newRow["psychosocial-support-community---primary-identified"] = "CASE";
                     break;
             if(codes_identified>=REQUIRED_CODES): break;
         if(codes_identified<REQUIRED_CODES):
-            newRow["psychosocial-support-counselling---primary-identified"] = "UNK";
+            newRow["psychosocial-support-community---primary-identified"] = "UNK";
         codes_identified=0;
         csv_writer.writerow(newRow)
